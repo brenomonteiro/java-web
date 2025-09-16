01ff38b108eceb829bbb33410dcb0c47d16b0fb9
Breno Mendes Monteiro <brennoo.mendesmonteiro@gmail.com>
2025-09-16 11:26:34 -0300

    feature-troca da página html estática por uma página dinâmica com o freemarker

--- crud-cidades/src/main/java/br/edu/utfpr/cp/espjava/crud_cidades/visao/CidadeController.java
+++ crud-cidades/src/main/java/br/edu/utfpr/cp/espjava/crud_cidades/visao/CidadeController.java
@@ -1,13 +1,26 @@
 package br.edu.utfpr.cp.espjava.crud_cidades.visao;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Set;
 
 @Controller
 public class CidadeController {
 
     @GetMapping("/")
-    public String listar(){
-        return "crud.html";
+    public String listar(Model memoria) {
+
+        var cidades = Set.of(
+                new Cidade("Cornélio Procópio", "PR"),
+                new Cidade("aSSIS", "SP"),
+                new Cidade("Itajaí", "SC")
+        );
+
+        memoria.addAttribute("listaCidades", cidades);
+        return "/crud";
     }
+
+
 }
